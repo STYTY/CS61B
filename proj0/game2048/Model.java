df2394a195f9d538250238b80b260afec15346a1
Sean-Jaii <SeanJaii@users.noreply.github.com>
2021-04-05 01:07:44 +0100

    project 0 part 1 - Empty Space

--- proj0/game2048/Model.java
+++ proj0/game2048/Model.java
@@ -5,7 +5,7 @@ import java.util.Observable;
 
 
 /** The state of a game of 2048.
- *  @author TODO: YOUR NAME HERE
+ *  @author SeanJaii
  */
 public class Model extends Observable {
     /** Current contents of the board. */
@@ -137,7 +137,14 @@ public class Model extends Observable {
      *  Empty spaces are stored as null.
      * */
     public static boolean emptySpaceExists(Board b) {
-        // TODO: Fill in this function.
+        for (int i = 0; i < b.size(); i++) {
+            for (int j = 0; j < b.size(); j++) {
+                if (b.tile(i, j) == null) {
+                    System.out.println("title doesn't exist at position: " + b.tile(i, j));
+                    return true;
+                }
+            }
+        }
         return false;
     }
 
